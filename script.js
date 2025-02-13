1eb9c0f8ed62ead3559c0e69fa086d0feb2c0b3f
Task View <maumauriciog@hotmail.com>
2025-02-13 17:15:26 -0300

    incluisão de icone, alteração arquivos .js e .css

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ let numeroDez = 10;
 let StringDez = '10';
 
 if (numeroUm === stringUm) {
-    console.log(`As variáveis numeroUm e stringUm, possuem o mesmo valor. `)
+    console.log(`As variáveis numeroUm e stringUm, possuem o mesmo valor. `);
 } else {
-    console.log(`As variáveis numeroUm e stringUm, não tem o mesmo valor. `)
+    console.log(`As variáveis numeroUm e stringUm, não tem o mesmo valor. `);
 };
+
+if(numeroTrinta === stringTrinta) {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo');
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo');
+};
+
+if(numeroDez === stringDez){
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes');
+} else {
+    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor');
+}
