6f59eced25ecb9ab536039a9f0348d3865a5ef06
Task View <maumauriciog@hotmail.com>
2025-02-13 16:25:49 -0300

    criando arquivos

---
+++ script.js
@@ -0,0 +1,15 @@
+// Comparando Valores e Tipos
+let numeroUm = 1;
+let stringUm = '1';
+
+let numeroTrinta = 30;
+let stringTrinta = '30';
+
+let numeroDez = 10;
+let StringDez = '10';
+
+if (numeroUm === stringUm) {
+    console.log(`As variáveis numeroUm e stringUm, possuem o mesmo valor. `)
+} else {
+    console.log(`As variáveis numeroUm e stringUm, não tem o mesmo valor. `)
+};
